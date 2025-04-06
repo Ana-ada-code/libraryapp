d88c6f92698cbb4698f3c3f21add13340d9f01f4
Ana <adamikana@gmail.com>
2025-04-06 22:19:36 +0200

    feat: add update-book

---
+++ src/app/update-book/update-book.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../book';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BookService} from '../book.service';
+
+@Component({
+  selector: 'app-update-book',
+  templateUrl: './update-book.component.html',
+  styleUrls: ['./update-book.component.css']
+})
+export class UpdateBookComponent implements OnInit {
+
+  id: number = 0;
+  book: Book = new Book(undefined!, '', '', '','');
+  submitted = false;
+
+  constructor(private route: ActivatedRoute, private router: Router,
+              private bookService: BookService) {
+  }
+
+  ngOnInit() {
+    this.book = new Book(undefined!, '', '', '','');
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.bookService.getBook(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.book = data;
+      }, error => console.log(error));
+  }
+
+  onSubmit(): void {
+    this.bookService.updateBook(this.book.id, this.book).subscribe(() => {
+      this.bookService.notifyBookListChanged();
+      this.submitted = true;
+      this.router.navigate(['/books']);
+    });
+  }
+
+}
